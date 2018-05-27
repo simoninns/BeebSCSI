74e5eea453e897091e0935d6f9b10b179c47d448
Simon Inns <simon.inns@gmail.com>
2018-05-27 09:20:31 +0200

    Enhanced status LED control to fix #8
    Status LED is now PWM controlled.  When power on the LED lights with minimum brightness.  Activity now causes the LED to show maximum brightness followed by a .25 second fade out.

--- BeebSCSI_AVR/BeebSCSI_AVR/statusled.c
+++ BeebSCSI_AVR/BeebSCSI_AVR/statusled.c
@@ -27,6 +27,7 @@
 // Global includes
 #include <avr/io.h>
 
+#include <avr/interrupt.h>
 #include <stdbool.h>
 #include <stdio.h>
 
@@ -35,15 +36,71 @@
 #include "debug.h"
 #include "statusled.h"
 
-// Initialise status LED (called on a cold-start of the AVR)
+// LED brightness can be between 0 (off) and 19 (on)
+volatile uint8_t ledActualBrightness = 0;
+volatile uint8_t ledTargetBrightness = 0;
+volatile uint8_t fadeCounter = 0;
+volatile uint8_t pwmCounter = 0;
+
+// Interrupt Service Routine based on Timer0 for LED PWM output
+ISR(TIMER2_COMPA_vect)
+{
+	// Set the LED to on or off depending on the required brightness level
+	if (ledActualBrightness > pwmCounter) STATUS_LED_PORT &= ~STATUS_LED; // Pin = 0 (on)
+	else STATUS_LED_PORT |= STATUS_LED; // Pin = 1 (off)
+	
+	// Update and range check the PWM counter
+	pwmCounter++;
+	if (pwmCounter > 19) pwmCounter = 0;
+	
+	// Perform fading control
+	if (ledTargetBrightness >= ledActualBrightness)
+	ledActualBrightness = ledTargetBrightness;
+	else
+	{
+		fadeCounter++;
+		if (fadeCounter == 12)
+		{
+			ledActualBrightness--;
+			fadeCounter = 0;
+		}
+	}
+}
+
+// Initialise status LED
 void statusledInitialise(void)
 {
 	// Configure the status LED
 	STATUS_LED_DDR |= STATUS_LED; // Output
 	STATUS_LED_PORT |= STATUS_LED; // Pin = 1 (off)
+	
+	// Configure Timer2 to interrupt (8-bit timer) for PWM output
+	//
+	// The required duty-cycle is 50Hz (20,000 us)
+	// The required number of brightness levels is 20
+	// This means we require an interrupt every 1,000 us
+	//
+	// fOSC is 16,000,000 ticks per second
+	// 16,000,000 / 64 prescale = 250,000 ticks per second
+	// 250,000 ticks per second = 1 tick per 4 us
+	// @4us per tick, 250 ticks = 1,000 us
+	//
+	OCR2A = 200; // 200 = 1,000 us
+	
+	// Set OC0A on Compare Match, clear OC0A at TOP - Fast PWM
+	TCCR2A = (1 << WGM22) | (1 << WGM21) | (1 << WGM20);
+	
+	// Set /64 clock prescale
+	TCCR2B = (1 << CS21) | (1 << CS20);
+	
+	// Enable timer0 interrupt
+	TIMSK2 = (1 << OCIE2A);
+	
+	// Set the initial brightness (note: lowest brightness is used to show "powered" status)
+	ledTargetBrightness = 1;
 }
 
-// Reset the status LED (called when the host signals reset)
+// Reset the status LED
 void statusledReset(void)
 {
 	// Turn the activity indication off
@@ -53,6 +110,6 @@ void statusledReset(void)
 // Show activity using the status LED
 void statusledActivity(uint8_t state)
 {
-	if (state == 1) STATUS_LED_PORT &= ~STATUS_LED; // Pin = 0 (on)
-	else STATUS_LED_PORT |= STATUS_LED; // Pin = 1 (off)
+	// If the activity light is on, set to full brightness
+	if (state == 1) ledTargetBrightness = 19; else ledTargetBrightness = 1;
 }
